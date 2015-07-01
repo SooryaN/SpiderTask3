190a91a510feae542dceb3c5eaf91451fb5c7c5e
SooryaN <nayaran.ayroos@gmail.com>
2015-07-01 20:01:11 +0530

    Configuration file for database

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$username = "root"; //mysql username
+$password = "php23528w!!"; //mysql password
+$hostname = "localhost"; //hostname
+$databasename = 'Spider3'; //databasename
+$tablename= 'Userdata';
+//connect to database
+$mysqli = new mysqli($hostname, $username, $password, $databasename);
+
+
+?>
