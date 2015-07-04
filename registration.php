0ac671181e01b629bffb7e9e04f0c89ee7066019
SooryaN <nayaran.ayroos@gmail.com>
2015-07-04 14:58:22 +0530

    easier to use and access

--- registration.php
+++ registration.php
@@ -1,117 +1,121 @@
 <?php
-	session_start();
-	$_SESSION = array();
-	
-	include("simple-php-captcha.php");
-	
+    session_start();
+    
+    
+    include("simple-php-captcha.php");
+    
 $_SESSION['captcha'] = simple_php_captcha( array(
-	'min_length' => 5,
-	'max_length' => 7,
-	'fonts' => array($_SERVER['DOCUMENT_ROOT'] . '/' .'REG/captcha/fonts/1.ttf',$_SERVER['DOCUMENT_ROOT'] . '/' . 'REG/captcha/fonts/2.ttf'),
-	'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
-	'min_font_size' => 20,
-	'max_font_size' => 28,
-	'color' => '#666',
-	'angle_min' => 0,
-	'angle_max' => 10,
-	'shadow' => true,
-	'shadow_color' => '#fff',
-	'shadow_offset_x' => -2,
-	'shadow_offset_y' => 1
+    'min_length' => 5,
+    'max_length' => 7,
+    'fonts' => array($_SERVER['DOCUMENT_ROOT'] . '/' .'REG/captcha/fonts/1.ttf',$_SERVER['DOCUMENT_ROOT'] . '/' . 'REG/captcha/fonts/2.ttf'),
+    'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
+    'min_font_size' => 20,
+    'max_font_size' => 28,
+    'color' => '#666',
+    'angle_min' => 0,
+    'angle_max' => 10,
+    'shadow' => true,
+    'shadow_color' => '#fff',
+    'shadow_offset_x' => -2,
+    'shadow_offset_y' => 1
 ));
-	
-	?>
+    
+    ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Register</title>
-		<link rel="stylesheet" href="css/main.css" type="text/css">
-		<link rel="stylesheet" href="css/base.css" type="text/css">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-		
+<head>
+  <meta charset="UTF-8">
 
-	<script type="text/javascript">
-	$(document).ready(function() {
+  <title>Register</title>
+  <link href="css/main.css" rel="stylesheet" type="text/css">
+  <link href="css/base.css" rel="stylesheet" type="text/css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  <script type="text/javascript">
+$(document).ready(function() {
 
-		$("#FormSubmit").click(function (e) {
-				e.preventDefault();
-								
-				$("#FormSubmit").hide(); 
-				$("#LoadingImage").show();
-				
-			 	var myData = {page:'Reg',name:$("#name").val(),username:$("#username").val(),password:$("#password").val(),captcha:$("#captcha").val()}; //build a post data structure
-				$.post('everything.php', myData, function(returnedData) {
-				    if(returnedData.search('successfully registered.')!=-1)
-				    {window.setTimeout(function(){window.location = "http://localhost/SpiderTask3/login.php"}, 1500);}
-				    $("#responds").html(returnedData);
-				    $("#FormSubmit").show(); 
-					$("#LoadingImage").hide();
-				});
-		});
-	});
-	</script>
-	</head>
-	<body class="rel">
+        $("#FormSubmit").click(function (e) {
+                e.preventDefault();
+                                
+                $("#FormSubmit").hide(); 
+                $("#LoadingImage").show();
+                
+                var myData = {page:'Reg',name:$("#name").val(),username:$("#username").val(),password:$("#password").val(),captcha:$("#captcha").val()}; //build a post data structure
+                $.post('everything.php', myData, function(returnedData) {
+                    if(returnedData.search('successfully registered.')!=-1)
+                    {window.setTimeout(function(){window.location = "http://localhost/SpiderTask3/login.php"}, 1500);}
+                    $("#responds").html(returnedData);
+                    $("#FormSubmit").show(); 
+                    $("#LoadingImage").hide();
+                });
+        });
+    });
+  </script>
+</head>
 
- 
+<body class="rel">
+<header id="page_header">
+<ul>
+<li><h1>Join the Club!</h1></li>
+<li><h1><a href="login.php">Login?</a></h1></li>
+</ul></header>
   <nav id="menu">
     <ul>
-      <li><h1><i class="fa fa-list"></i> Menu</h1></li>
-      <li><a href="index.php"><i class="fa fa-check"></i> Home</a></li>
-      <li><a href="login.php"><i class="fa fa-check"></i> Login</a></li>
-      <li><a href="registration.php"><i class="fa fa-check"></i>Register</a></li>
-      <li><a href="#paragraph4"><i class="fa fa-check"></i> Paragraph 4</a></li>
-      <li><a href=""><i class="fa fa-check"></i> Back</a></li>
+      <li>
+        <h1><i class="fa fa-list"></i> Menu</h1>
+      </li>
+
+
+      <li>
+        <a href="index.php"><i class="fa fa-check"></i> Home</a>
+      </li>
+
+
+      <li>
+        <a href="login.php"><i class="fa fa-check"></i>Login/Register</a>
+      </li>
+
+      <li>
+        <a href="submit.php"><i class="fa fa-check"></i> Submit Qs</a>
+      </li>
+
+
+        <li> <a href="answer.php"><i class="fa fa-check"></i> Answer Qs</a> </li>
+      <li> <a onclick="function(e){e.preventDefault();this.parents('header').hide();}"><i class="fa fa-check"></i> Exit</a> </li>
+    
     </ul>
   </nav>
-  <button href="#" class="menu-btn"><i class="fa fa-bars fa-3x"></i></button>
-  <script src="navbar.js"></script>
-</div>
-		<div style="width: 500px;margin:auto;" id="form">
-		
-			<div id="title">
-				<h1>Registration Form </h1>
-			</div >
-			<ol>
-				<li>
-					Name:
-					<br>
-					<input type="text" id="name" placeholder="Enter your name" required/>
-					<br>
-				</li>
-				<li>
-					Username:
-					<br>
-					<input type="text"  id="username" placeholder="Unique Username"  required/>
-					<br>
-				</li>
-				<li>
-					Password:
-					<br>
-					<input type="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,})" required placeholder="At least 1 lowercase, 1 uppercase, 1 number, min length 6" name="pass" id="password" autocomplete="off"/>
-					<input type="checkbox" style="margin-top:0;" onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"> Show password
-					<br>
-				</li>
-				<br>
-				<li>
-					<label for="captcha">Enter the text:</label>
-					<br>
-					<?php
-						echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code">';
-						
-					?>
-					<input type="text" id="captcha" name="captcha" autocomplete="off" required/>
+  <button class="menu-btn fa fa-bars fa-3x"></button> <script src="navbar.js"></script>
 
-					<br>
-				</li>
-				<div id="responds"></div>
-				<br>
-				<input type="submit" value="Register" id="FormSubmit" name="submit">
-				<img src="images/loading.gif" id="LoadingImage" style="display:none" />
-				<br><br>
-			</ol>
-		</div>
-	</body>
+  <div id="form">
+    <div id="title">
+      <h1>Registration Form</h1>
+    </div>
+      Name:<br>
+      <input id="name" placeholder="Enter your name" type="text"><br>
+
+      Username:<br>
+      <input id="username" placeholder="Unique Username" type="text"><br>
+
+      Password:<br>
+      <input autocomplete="off" id="password" name="pass" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,})" placeholder="At least 1 lowercase, 1 uppercase, 1 number, min length 6" required="" type="password"> <input onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"  type="checkbox"> Show password<br>
+      <br>
+
+      <label for="captcha">Enter the text:</label><br>
+        <?php
+                                        echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code">';
+                                        
+                                    ?> <input autocomplete="off" id="captcha" name="captcha" type="text"><br>
+
+
+        <div id="responds">
+        </div>
+        <br>
+        <input id="FormSubmit" name="submit" type="submit" value="Register"> <img id="LoadingImage" src="images/loading.gif" style="display:none"><br>
+        <br>
+        <div id="nregister">
+      Already Registered? <a href="login.php">Login here!</a>
+      </div>
+  </div>
+</body>
 </html>
